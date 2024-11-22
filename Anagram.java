ae5b8929eff4e664ee873300233bf8e48a2dedfc
TamarHasar <tamar.hasar@post.runi.ac.il>
2024-11-22 23:04:02 +0200

    commit to check if it works

--- Anagram.java
+++ Anagram.java
@@ -4,6 +4,7 @@ public class Anagram {
 	public static void main(String args[]) {
 		// Tests the isAnagram function.
 		System.out.println(isAnagram("silent","listen"));  // true
+		System.out.println(isAnagram("hello","hello!"));  // true
 		System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
 		System.out.println(isAnagram("Madam Curie","Radium came")); // true
 		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
@@ -29,22 +30,72 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		str1 = preProcess(str1);
+		str2 = preProcess(str2);
+
+		str1 = str1.replace(" ", "");
+		str2 = str2.replace(" ", "");
+
+
+		boolean isSameChar = false;
+
+		if (str1.length() != str2.length()) {
+			return false;
+		}
+
+		for(int i = 0; i < str1.length(); i ++) {
+			isSameChar = false;
+
+			for (int j = 0; j < str2.length(); j++) {
+				if (str1.charAt(i) == str2.charAt(j)) {
+					isSameChar = true;
+
+					str2 = str2.substring(0, j) + str2.substring(j + 1);
+					str1 = str1.substring(0, i) + str1.substring(i + 1);
+				}
+			}
+
+			if (!isSameChar) {
+				return false; 
+			}
+		}
+
+		return true;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return str;
+		String revisedStr = "";
+
+		for (int i = 0; i < str.length(); i++) {
+
+			if (Character.isLetter(str.charAt(i))) {
+				revisedStr += Character.toLowerCase(str.charAt(i));
+			}
+			
+			else if (str.charAt(i) == ' ') {
+				revisedStr += str.charAt(i);
+			}
+		}
+
+		return revisedStr;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return str;
+		String anagram = "";
+		int length = str.length(); 
+		int randomIndex = 0;
+
+		for (int i = 0; i < length; i ++) {
+			randomIndex = (int) (Math.random() * str.length());
+			anagram += str.charAt(randomIndex);
+			str = str.replaceFirst(Character.toString(str.charAt(randomIndex)), "");
+		}
+
+		return anagram;
 	}
 }
